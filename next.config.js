ea90b2d18431c61f111c65a2e38ed15ea99a548d
gofaiz23-dotcom <gofaiz23@gmail.com>
2026-04-24 23:03:06 +0530

    first commit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
+const path = require("path");
+
 /** @type {import('next').NextConfig} */
 const BASE_URL = process.env.BASE_URL || "http://localhost:8005";
 
 const nextConfig = {
   reactStrictMode: true,
+  outputFileTracingRoot: path.resolve(__dirname),
   async rewrites() {
     return [
       {
